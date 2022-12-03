1a2b3daf12a3a4d9458242a36ec85a1ae3a634d7
Nathanael Kastner <nmk9@calvin.edu>
2022-12-02 21:44:22 -0500

    initial commit

---
+++ Final_Project/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'essential-oils-inventory',
+    appId: '1:437740671415:web:24cfdece0aa1ca5aa51832',
+    storageBucket: 'essential-oils-inventory.appspot.com',
+    apiKey: 'AIzaSyBsWr-HGEn2Fgvm8GsfMFSGH_EjRhhX6a8',
+    authDomain: 'essential-oils-inventory.firebaseapp.com',
+    messagingSenderId: '437740671415',
+  },
+  production: true
+};
