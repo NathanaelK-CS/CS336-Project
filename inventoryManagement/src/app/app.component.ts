5c358f7ede8d81603760eb674f8e80b8f32eb900
Andrew Hill <ashill1470@gmail.com>
2022-12-04 21:13:52 -0500

    Table created

--- inventoryManagement/src/app/app.component.ts
+++ inventoryManagement/src/app/app.component.ts
@@ -1,5 +1,26 @@
 import { Component } from '@angular/core';
 
+interface essentialOil {
+  product: string;
+  description: string;
+  uses: string;
+  benefits: string;
+}
+
+const peppermint: essentialOil = {
+  product: "Peppermint",
+  description: "smells nice",
+  uses: "headaches",
+  benefits: "smells good"
+}
+
+const lavender: essentialOil = {
+  product: "Lavender",
+  description: "Comes from lavender plant",
+  uses: "sleep",
+  benefits: "promotes relaxation"
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +28,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inventoryManagement';
+  myData = [peppermint, lavender];
+  columnsToDisplay = ['product', 'description', 'uses', 'benefits']
+
 }
