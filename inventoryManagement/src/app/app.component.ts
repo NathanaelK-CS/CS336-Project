76c6f93f107ebb324d0dbc459771f3462ff5176f
Andrew Hill <ashill1470@gmail.com>
2022-12-06 16:36:14 -0500

    Added additional table functionality

--- inventoryManagement/src/app/app.component.ts
+++ inventoryManagement/src/app/app.component.ts
@@ -5,20 +5,25 @@ interface essentialOil {
   description: string;
   uses: string;
   benefits: string;
+  count: Number;
 }
 
+// For testing
 const peppermint: essentialOil = {
   product: "Peppermint",
   description: "smells nice",
   uses: "headaches",
-  benefits: "smells good"
+  benefits: "smells good",
+  count: 2
 }
 
+// For testing
 const lavender: essentialOil = {
   product: "Lavender",
   description: "Comes from lavender plant",
   uses: "sleep",
-  benefits: "promotes relaxation"
+  benefits: "promotes relaxation",
+  count: 3
 }
 
 @Component({
@@ -29,6 +34,17 @@ const lavender: essentialOil = {
 export class AppComponent {
   title = 'inventoryManagement';
   myData = [peppermint, lavender];
-  columnsToDisplay = ['product', 'description', 'uses', 'benefits']
+  columnsToDisplay = ['count', 'product', 'description', 'uses', 'benefits', 'icon']
 
+  addNew() {
+    console.log("Button Press is working");
+  }
+
+  editRow() {
+    console.log("Edit Button works");
+  }
+
+  deleteRow() {
+    console.log("Delete button works");
+  }
 }
--- inventoryManagement/src/app/app.module.ts
+++ inventoryManagement/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { MatTableModule } from '@angular/material/table';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
